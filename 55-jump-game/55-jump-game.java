a081646273f767bc19921719e7f1e5c5058e9627
Ishmoth Ura Nuri <81003608+Rangerslider@users.noreply.github.com>
2022-06-27 11:53:30 +0600

    Time: 3 ms (63.35%), Space: 67.9 MB (49.49%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int  lastIndexPosition = nums.length -1 ;
+        for (int i = nums .length -1 ; i>=0; i--)
+        {
+            if (i+  nums[i] >= lastIndexPosition){
+                
+                lastIndexPosition = i;
+            }
+        }
+         return lastIndexPosition  == 0;
+    }
+    
+}
