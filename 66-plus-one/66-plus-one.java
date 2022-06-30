28d1989cebc73f1aa6649e980e2700440aaf7481
Ishmoth Ura Nuri <81003608+Rangerslider@users.noreply.github.com>
2022-06-30 16:11:09 +0600

    Time: 0 ms (100.00%), Space: 42.6 MB (43.80%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+         
+        int a = digits.length ;
+        for( int i=a-1; i>=0; i--){
+            
+            if (digits[i]<9){
+                digits [i]++;
+                return digits;  
+            } 
+            digits [i] = 0;
+        
+        }
+        int[]new_number =new int [a+1 ];
+        new_number [0] =1;
+        return new_number ;
+        
+    }
+}
