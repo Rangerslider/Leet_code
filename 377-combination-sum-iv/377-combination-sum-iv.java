73c222770efbcc563bebb022060aa1172ba3bade
Ishmoth Ura Nuri <81003608+Rangerslider@users.noreply.github.com>
2022-08-05 10:07:08 +0600

    Time: 2 ms (65.18%), Space: 41.2 MB (67.69%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+    int[] c= new int[target + 1];
+    c[0] = 1;
+    for (int i = 1; i < c.length; i++) {
+        for (int j = 0; j < nums.length; j++) {
+            if (i - nums[j] >= 0) {
+                c[i] += c[i - nums[j]];
+            }
+        }
+    }
+    return c[target]; 
+    }
+}
