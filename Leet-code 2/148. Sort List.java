ad9b79fa96e62dc79ba87a29d993b679fe239fd0
Nuri <nuri1999sept@gmail.com>
2023-02-21 22:15:04 +0600

    0.53

---
+++ Leet-code 2/148. Sort List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+        
+        ListNode ans= new ListNode(list.get(0));
+        for(int i=1; i< list.size(); i++){
+            ans = new ListNode(list.get(i), ans);
+        }
+        
+        return ans;
+        
+    }
+}
