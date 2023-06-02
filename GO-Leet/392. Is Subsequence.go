0ca4be27bfef2ed4c7e1e63377f4620f3c86fd7b
nuri <nuri1999sept@gmail.com>
2023-06-02 19:26:47 +0600

    0.48

---
+++ GO-Leet/392. Is Subsequence.go
@@ -0,0 +1,19 @@
+package main
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	for i := 0; i < len(t); i++ {
+		if t[i] == s[0] {
+			s = s[1:]
+		}
+
+		if len(s) == 0 {
+			return true
+		}
+	}
+
+	return false
+}
